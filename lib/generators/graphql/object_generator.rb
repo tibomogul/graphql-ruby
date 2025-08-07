13a5dc616ea109ab71455011c83c55b20ffb41b3
Tibo Mogul <tibo.mogul@valuepro.com.au>
2025-08-07 16:02:53 +1000

    require the new file

--- lib/generators/graphql/object_generator.rb
+++ lib/generators/graphql/object_generator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'generators/graphql/type_generator'
 require 'generators/graphql/field_extractor'
+require 'generators/graphql/relationship_extractor'
 
 module Graphql
   module Generators
